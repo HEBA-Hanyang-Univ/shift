2dcab3f94e7ef40b585cf5b194b60872973a4185
gnstjdwh2 <90314280+gnstjdwh2@users.noreply.github.com>
2023-11-21 12:38:49 +0900

    Update question, sq

--- flask/question.py
+++ flask/question.py
@@ -1,15 +1,15 @@
-
 class Question :
     def __init__(self):
         f = open("resources/question_list.txt", "r")
         self.question_list = [elem for elem in f.read().splitlines() if len(elem) > 5]
-        print('read question_list.txt file successful') 
         f.close()
+        self.question_dict = dict()
+        for i in range(0, len(self.question_list)):
+            self.question_dict[i+1] = self.question_list[i]
 
-    def get_list(self):
-        return self.question_list
+    def get_dic(self):
+        return self.question_dict
 
-
-if __name__ == "__main__" :
-    q = Question()
-    print(q.get_list())
+# if __name__ == "__main__" :
+#     q = Question()
+#     print(q.get_dic())
