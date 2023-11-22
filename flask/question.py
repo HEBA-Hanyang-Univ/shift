b893aef824d60ad22474e62718410de70cea447d
JUNStone <j00mst0m3@gmail.com>
2023-11-23 03:25:18 +0900

    modify Question.py - change txt to json

--- flask/question.py
+++ flask/question.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 class Question :
     def __init__(self):
         f = open("resources/question_list.txt", "r")
@@ -6,6 +7,34 @@ class Question :
         self.question_dict = dict()
         for i in range(0, len(self.question_list)):
             self.question_dict[i+1] = self.question_list[i]
+=======
+import json
+import os
+
+my_dir = os.path.dirname(__file__)
+json_file = os.path.join(my_dir, 'question_list.json')
+
+def read_json_file(file_name):
+    try:
+        with open(file_name, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            print('read {file_name} file successful')
+    except FileNotFoundError:
+        data = []
+        with open(json_file, "w", encoding="utf-8") as f:
+            json.dump(data, f)
+            print('error while reading {file_name} file, so create a new {file_name}')
+    return data
+
+class Question :
+    def __init__(self):
+        self.question_list = read_json_file('question_list.json')
+        print(self.question_list)
+        # f = open("resources/question_list.txt", "r")
+        # self.question_list = [elem for elem in f.read().splitlines() if len(elem) > 5]
+        # print('read question_list.txt file successful')
+        # f.close()
+>>>>>>> Stashed changes
 
     def get_dic(self):
         return self.question_dict
