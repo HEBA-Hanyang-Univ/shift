ee09cdea33bca280e92ea31a66bda43cab277484
bigmooon <bigmooon1215@gmail.com>
2023-11-23 05:32:58 +0900

    modify some Footer

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -5,11 +5,9 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Transition } from "react-transition-group";
 
-export const Footer = ({ link }) => {
+export const Footer = ({ link, helpContent }) => {
   const [showHelpSlide, setShowHelpSlide] = useState(false);
   const [showOpenBtn, setShowOpenBtn] = useState(true);
-
-  const slideContent = "자신에게 맞는 또는 가장 가까운 것 같은 MBTI를 최대 2개까지 선택할 수 있습니다.";
 
   const duration = 300;
 
@@ -25,6 +23,11 @@ export const Footer = ({ link }) => {
     exiting: {maxHeight: 0},
     exited: {maxHeight: 0},
   };
+
+  const onClick = (e) => { 
+    // TODO: 전송할 데이터 설정
+    e.preventDefault();
+  }
 
   const handleOpen = () => {
     setShowHelpSlide(true);
@@ -52,13 +55,13 @@ export const Footer = ({ link }) => {
             ...defaultStyle,
             ...transitionStyles[state]
           }}>
-            <FooterHelp content={slideContent} hideHelpSlide={handleClose}></FooterHelp>
+            <FooterHelp helpContent={helpContent} hideHelpSlide={handleClose}></FooterHelp>
           </div>
         )}
       </Transition>
       <div className="footerContainer">
         <button>
-          <Link to={link} style={{textDecoration: 'none'}}>
+          <Link to={link} style={{textDecoration: 'none'}} onClick={(event) => onClick(event)}>
             <span className="footerContainerSpan">
               NEXT
             </span>
