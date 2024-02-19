25553d08227a4548fed3d5c4d2ec38d14650ef61
bigmooon <bigmooon1215@gmail.com>
2024-02-20 05:02:27 +0900

    FE - upload Button Component  and modified common scss

--- react/src/components/Button/Button.js
+++ react/src/components/Button/Button.js
@@ -1,6 +1,13 @@
 // Button Modal uses through entire website
 import React from "react";
-import "./Button.scss";
 
 // 전체적인 버튼 (높이, 넓이, 배경색 유동적으로)
+export const Button = ({ color, gradient, width, height, className, children }) => {
+  const style = {
+    background: gradient ? `linear-gradient(${gradient})` : color, 
+    width: `${width}rem`,
+    height: `${height}rem`
+  };
 
+  return <button style={style} className={className}>{children}</button>;
+};
