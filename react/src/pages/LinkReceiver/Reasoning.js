aed946ac160ffaf8a44814a22b5e602e8150d295
bigmooon <bigmooon1215@gmail.com>
2024-02-27 19:12:12 +0900

    FE - upload for save

--- react/src/pages/LinkReceiver/Reasoning.js
+++ react/src/pages/LinkReceiver/Reasoning.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "../../assets/styles/LinkReceiver/Reasoning.scss";
 
-const Reasoning = () => {
+export const Reasoning = () => {
 
   return (
-    <div>키워드 선택 이유</div>
+    <div id="Container" className="reContainer">
+      <div className="reWrapper">
+        <div className="reTitleWrapper">
+          <span className="reTitleBlack">키워드를 선택한 </span>
+          <span className="reTitlePurple">이유</span>
+          <span className="reTitleBlack">나</span>
+          <br/>
+          <span className="reTitlePurple">일화</span>
+          <span className="reTitleBlack">가 있다면 적어보세요.</span>
+        </div>
+        <div className="reReasoningBox">
+
+        </div>
+      </div>
+    </div>
   )
 };
-
-export default Reasoning; 
