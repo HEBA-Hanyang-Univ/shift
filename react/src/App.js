1075caa63fa2f88a2747878b3f74143f8edb58b8
bigmooon <bigmooon1215@gmail.com>
2024-02-27 19:01:31 +0900

    FE - create landing page logic

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect, React } from "react";
+import { useEffect, React, useState } from "react";
 import { Header } from "./components/Header/Header";
 import { StartHost } from "./pages/LinkSender/StartHost";
 import { InfoHost } from "./pages/LinkSender/InfoHost";
@@ -9,6 +9,9 @@ import { CompleteHost } from "./pages/LinkSender/CompleteHost";
 import { LandingGuest } from "./pages/LinkReceiver/LandingGuest";
 import { StartGuest } from "./pages/LinkReceiver/StartGuest";
 import { InfoGuest } from "./pages/LinkReceiver/InfoGuest";
+import { Reasoning } from "./pages/LinkReceiver/Reasoning";
+import { OneLineDescription } from "./pages/LinkReceiver/OneLineDescription";
+import { CompleteGuest } from "./pages/LinkReceiver/CompleteGuest";
 
 function App() {
   const ScrollToTop = () => {
@@ -20,7 +23,7 @@ function App() {
 
     return null;
   };
- 
+
   return (
     <div className="App">
       <Router>
@@ -31,12 +34,19 @@ function App() {
           <Route path="/startHost" element={<StartHost/>}></Route>
           <Route path="/inputInfo" element={<InfoHost/>}></Route> 
           <Route path="/completeHost" element={<CompleteHost/>}></Route>
+
+
           <Route path="/myIdentity" element={<MyIdentity/>}></Route>
 
           {/* LinkReceiver */}
           <Route path="/landing" element={<LandingGuest/>}></Route>
           <Route path="/startGuest" element={<StartGuest/>}></Route>
-          <Route path="/infoGuest" element={<InfoGuest/>}></Route>
+          <Route path="/reasoning" element={<Reasoning/>}></Route>
+          <Route path="/oneLineDescription" element={<OneLineDescription/>}></Route>
+          <Route path="/completeGuest" element={<CompleteGuest/>}></Route>
+
+
+          <Route path="/infoGuest" element={<InfoGuest/>}></Route> 
         </Routes>
       </Router>
     </div>
--- react/src/pages/LinkReceiver/StartGuest.js
+++ react/src/pages/LinkReceiver/StartGuest.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../../assets/styles/common.scss";
 import "../../assets/styles/LinkReceiver/StartGuest.scss";
 import { Button } from "../../components/Button/Button";
+import { LandingGuest } from "./LandingGuest";
 
 export const StartGuest = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);   // 2초 후에 로딩 상태 변경
+  }, []);
+
+  if(isLoading) {
+    return <LandingGuest />;
+  }
 
   return (
     <div id="Container" className="sgContainer">
@@ -16,7 +29,7 @@ export const StartGuest = () => {
           <Button gradient="180deg, #9C76AC 0%, #AC86BD 100%" width={8} height={3} className="sgStartBtn">시작하기</Button>
           <span>지금까지 5,800명이 참여했어요!</span>
         </div>          
-      </div>
+      </div> 
       <div className="sgFooter">
         <span>평균 소요시간 : 2분</span>
       </div>
