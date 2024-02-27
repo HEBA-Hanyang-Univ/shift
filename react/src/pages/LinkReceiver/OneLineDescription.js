8470e0999632d1adeea4c1997ddfe98aae9e9270
bigmooon <bigmooon1215@gmail.com>
2024-02-27 19:02:48 +0900

    FE - upload oneLineDescription page

--- react/src/pages/LinkReceiver/OneLineDescription.js
+++ react/src/pages/LinkReceiver/OneLineDescription.js
@@ -1,11 +1,49 @@
 import React from "react";
 import "../../assets/styles/LinkReceiver/OneLineDescription.scss";
+import "../../assets/styles/common.scss";
 
-const OneLineDescription = () => {
+// TODO : username 받아오기
+export const OneLineDescription = ({username}) => {
   
    return (
-    <div>한 줄 소개</div>
+    <div id="Container">
+      <div className="odWrapper">
+         <div className="odTitleWrapper">
+            <div className="odTitlePurple">{username}</div>
+            <div className="odTitleBlack">님은 한 줄로 소개하자면<br/>어떤 사람인가요?</div>
+         </div>
+         <div className="odInputWrapper">
+            <span className="odInputTop">USERNAME은</span>
+            <br/>
+            {/* TODO : 사용자 입력값 처리 */}
+            <input className="odInputMiddle" placeholder="사람이지만 그냥 돌인척 하는 얘"/>
+            <br/>
+            <span className="odInputBottom">이다.</span>
+         </div>
+         <div className="odExWrapper">
+            <div className="odExTitle">
+               <span>여러분을 도와줄 예시입니다!</span>
+            </div>
+            <div className="odExBox">
+               <div className="odEx1">
+                  <span className="odSpanBlack">홍길동은 </span>
+                  <span className="odSpanPurple">나이스한 미친놈</span>
+                  <span className="odSpanBlack">이다.</span>
+               </div> 
+               <div className="odEx2">
+                  <span className="odSpanBlack">엄복동은 </span>
+                  <span className="odSpanPurple">김치찌개를 잘 끓이는 바보</span>
+                  <span className="odSpanBlack">이다.</span>
+               </div>   
+               <div className="odEx3">
+                  <span className="odSpanBlack">신데렐라는 </span>
+                  <span className="odSpanPurple">12시까지 집에 가야하는 통금바보</span>
+                  <span className="odSpanBlack">이다.</span>
+               </div>
+            </div>
+         </div>
+      </div>
+    </div>
    ) 
 };
-
-export default OneLineDescription;
+  
