1084ac9ae05783d86dee625964eb0ffbebfa3b66
bigmooon <bigmooon1215@gmail.com>
2024-03-05 15:37:32 +0900

    FE - upload fonts

--- react/src/index.js
+++ react/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "./assets/fonts/WantedSans.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
