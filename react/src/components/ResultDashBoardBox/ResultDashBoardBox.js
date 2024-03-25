5d629bca0b38cac53a91cb59f98f61d54ddfa275
bigmooon <bigmooon1215@gmail.com>
2024-03-25 23:31:04 +0900

    update Result page

--- react/src/components/ResultDashBoardBox/ResultDashBoardBox.js
+++ react/src/components/ResultDashBoardBox/ResultDashBoardBox.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Button } from "../Button/Button";
+import MZImg from "../../assets/images/Result_MZ.svg";
 import "./ResultDashBoardBox.scss";
 
 export const ResultDashBoardBox = () => {
   return (
     <div className="rdbbWrapper">
-      
+      <div className="rdbbContentWrapper">
+        
+      </div>
+      <div className="rdbbBoxBtnWrapper">
+        <Button width={3} height={1} className="rbbPurpleBtn">
+          <span>결과 확인하기</span>
+        </Button>
+        <Button></Button>
+      </div>
     </div>
   )
 
--- react/src/pages/Result/ResultDashBoard.js
+++ react/src/pages/Result/ResultDashBoard.js
@@ -17,7 +17,6 @@ export const ResultDashBoard = () => {
           <PreparingDashBoardBox></PreparingDashBoardBox>
         </div>
       </div>
-
     </div>
   )
 }
