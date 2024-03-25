bce4e9a2e4d4968cc0cfd4559bec54ac504918c3
bigmooon <bigmooon1215@gmail.com>
2024-03-25 23:32:17 +0900

    update Footer navigation

--- react/src/components/Footer/GuestFooter.js
+++ react/src/components/Footer/GuestFooter.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Footer.scss";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from "../Button/Button";
-import { Dot } from "./Dot";
 import NextArrow from "../../assets/images/nextArrow.svg";
 
 export const GuestFooter = ({ prevPageUrl, nextPageUrl, isNextEnabled }) => {
+  const navigate = useNavigate();
+
+  const handleNextPage = () => {
+    if(isNextEnabled) {
+      navigate(nextPageUrl);
+    }
+  };
 
   return (
     <div className="guestFooterContainer">
@@ -13,23 +20,19 @@ export const GuestFooter = ({ prevPageUrl, nextPageUrl, isNextEnabled }) => {
         <div className="prevBtnBox">
           {prevPageUrl && 
             <Link to={prevPageUrl} style={{textDecoration:'none'}}>
-              <Button color='#9C76AC' width={5} height={2.5} className="prevPageBtn">
+              <Button color='#9C76AC' width={5} height={2} className="prevPageBtn">
                 이전
               </Button>
             </Link>}
         </div>
 
         <div className="nextBtnBox">
-          {nextPageUrl && 
-            <Link to={nextPageUrl} style={{textDecoration:'none'}}>
-              <Button color='#9C76AC' width={5} height={2} className="nextPageBtn" disabled={!isNextEnabled} >
-                다음
-                <img src={NextArrow} alt="nextBtn"/>
-              </Button>
-            </Link>}
+          <Button color='#9C76AC' width={5} height={2} className="nextPageBtn" onClick={handleNextPage} disabled={!isNextEnabled} >
+            다음
+            <img src={NextArrow} alt="nextBtn"/>
+          </Button>
         </div>
       </div>
     </div>
   )
 }
-  
--- react/src/pages/LinkSender/InfoHost.js
+++ react/src/pages/LinkSender/InfoHost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../assets/styles/LinkSender/InfoHost.scss";
 import { RadioBtn } from "../../components/Button/RadioBtn";
 import { DropDownBtn } from "../../components/Button/DropDownBtn";
@@ -6,6 +6,33 @@ import { GuestFooter } from "../../components/Footer/GuestFooter";
 
 export const InfoHost = () => {
   const ageOptions = Array.from({ length: 51}, (_, i) => ({key:i, value: `${i + 10}세`}));
+
+  const [name, setName] = useState('');
+  const [gender, setGender] = useState(null);
+  const [age, setAge] = useState('');
+  const [alarm, setAlarm] = useState(null);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleGenderChange = (value) => {
+    setGender(value);
+  }
+
+  const handleAgeChange = (value) => {
+    setAge(value);
+  }
+
+  const handleAlarmChange = (value) => {
+    setAlarm(value);
+  }
+
+  const [isNextEnabled, setIsNextEnabled] = useState(false);
+
+  useEffect(() => {
+    setIsNextEnabled(name.trim() !== '' && gender !== null && age !== '' && alarm !== null);
+  }, [name, gender, age, alarm]);
   
   return (
     <div id="Container">
@@ -17,20 +44,20 @@ export const InfoHost = () => {
           <span id="infoTitle">
             이름 또는 별명을 입력하세요.
           </span>
-          <input placeholder="난준석그냥돌"></input>
+          <input placeholder="난준석그냥돌" onChange={handleNameChange}></input>
         </div>
         <div className="ihInputGender">
           <span id="infoTitle">
             성별을 선택해주세요.
           </span>
-          <RadioBtn option1Text={'남자'} option2Text={'여자'} />
+          <RadioBtn option1Text={'남자'} option2Text={'여자'}  onChange={handleGenderChange}/>
         </div>
         <div className="ihInputAge">
           <span id="infoTitle">
             연령을 선택해주세요.
           </span>
           {/* TODO: 추후 옵션 데이터 전달 */}
-          <DropDownBtn options={ageOptions} placeholder="나이" onChange={(value) => console.log(value)}></DropDownBtn>
+          <DropDownBtn options={ageOptions} placeholder="나이" onChange={handleAgeChange} ></DropDownBtn>
         </div>
         <div className="ihPushAgree">
           <span id="infoTitle">알림톡 발송 동의</span>
@@ -39,13 +66,14 @@ export const InfoHost = () => {
             <br></br>
             결과를 전달해드립니다.
           </span>
-          <RadioBtn option1Text={'동의'} option2Text={'비동의'} />
+          <RadioBtn option1Text={'동의'} option2Text={'비동의'} onChange={handleAlarmChange} />
           <span className="ihPushAgreeNotice">
             테스트 결과 제공만을 위한 정보 수집입니다.
           </span>
         </div>
       </div>
-      <GuestFooter/> 
+      {/* TODO: 추후 url 수정 */}
+      <GuestFooter prevPageUrl={"/"} nextPageUrl={"/myIdentity"} isNextEnabled={isNextEnabled} /> 
     </div>
   )
 };
