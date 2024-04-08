a869ee3ebe41d6d8e11747a2bd477ab1a80e0081
bigmooon <bigmooon1215@gmail.com>
2024-04-09 05:05:21 +0900

    add Step component in GuestFooter component

--- react/src/components/Footer/GuestFooter.js
+++ react/src/components/Footer/GuestFooter.js
@@ -1,16 +1,43 @@
-import { useState } from "react";
 import "./Footer.scss";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Button } from "../Button/Button";
+import { StepNavigation } from "./StepNavigation";
 import NextArrow from "../../assets/images/nextArrow.svg";
+import { useState } from "react";
 
 export const GuestFooter = ({ prevPageUrl, nextPageUrl, isNextEnabled }) => {
   const navigate = useNavigate();
 
+  const stepPaths = [
+    {label: '', path: '/guest/info'},
+    {label: '', path: '/guest/keyword'},
+    {label: '', path: '/guest/reasoning'},
+    {label: '', path: '/guest/description'},
+  ];
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentStep, setCurrentStep] = useState(1);
+  
+  const updateStep = (step) => {
+    setCurrentStep(step);
+  };
+
+  const handlePrevPage = () => {
+    if(prevPageUrl) {
+      const prevStepIndex = stepPaths.findIndex(step => step.path === prevPageUrl);
+      setCurrentPage(prevStepIndex);
+      navigate(prevPageUrl);
+      setCurrentPage(prevStepIndex);
+    }
+  };
+
   const handleNextPage = () => {
     if(isNextEnabled) {
+      const nextStepIndex = stepPaths.findIndex(step => step.path === nextPageUrl);
+      setCurrentStep(nextStepIndex);
       navigate(nextPageUrl);
+      setCurrentPage(nextStepIndex);
     }
   };
 
@@ -19,7 +46,7 @@ export const GuestFooter = ({ prevPageUrl, nextPageUrl, isNextEnabled }) => {
       <div className="footerWrapper">
         <div className="prevBtnBox">
           {prevPageUrl && 
-            <Link to={prevPageUrl} style={{textDecoration:'none'}}>
+            <Link to={prevPageUrl} style={{textDecoration:'none'}} onClick={handlePrevPage}>
               <Button color='#9C76AC' width={5} height={2} className="prevPageBtn">
                 이전
               </Button>
