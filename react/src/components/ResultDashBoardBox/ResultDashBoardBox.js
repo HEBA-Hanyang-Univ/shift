4489ab71c30502d133b9619afbd17ea8dbce3bf6
bigmooon <bigmooon1215@gmail.com>
2024-04-09 19:39:19 +0900

    add css to Dashboard component

--- react/src/components/ResultDashBoardBox/ResultDashBoardBox.js
+++ react/src/components/ResultDashBoardBox/ResultDashBoardBox.js
@@ -5,15 +5,28 @@ import "./ResultDashBoardBox.scss";
 
 export const ResultDashBoardBox = () => {
   return (
-    <div className="rdbbWrapper">
-      <div className="rdbbContentWrapper">
-        
-      </div>
-      <div className="rdbbBoxBtnWrapper">
-        <Button width={3} height={1} className="rbbPurpleBtn">
-          <span>결과 확인하기</span>
-        </Button>
-        <Button></Button>
+    <div className="rdbbContainer">
+      <div className="rdbbWrapper">
+        <div className="rdbbContentWrapper">
+          <div className="rdbbContentLeft">
+            <div className="rdbbContentTitle">
+              <span>MZ 자기객관화 테스트</span>
+            </div>
+            <div className="rdbbContentSubTitle">
+              <span>응답자 수 &nbsp;&nbsp;&nbsp;</span>
+              <span>1 / 3</span>
+            </div>
+          </div>
+          <div className="rdbbContentRight">
+            <img src={MZImg} alt="MZImg"/>
+          </div>
+        </div>
+        <div className="rdbbBoxBtnWrapper">
+          <Button width={3} height={1} className="rbbPurpleBtn" color={"#9C76AC"}>
+            <span>결과 확인하기</span>  
+          </Button>
+          <Button></Button>
+        </div>
       </div>
     </div>
   )
