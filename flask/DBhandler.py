6b0f715864807a11a591bc3b946211e04db80ba7
JUNStone <j00mst0m3@gmail.com>
2024-04-10 02:27:25 +0900

    Implement kakao social login and DB model

---
+++ flask/DBhandler.py
@@ -0,0 +1,51 @@
+# YOU MUST MODIFY pyrebase.py due to gcloud error.
+# gcloud is no longer supported, so you need to modify pyrebase.py
+# install google-cloud-storage and modify pyrebase.py
+# from gcloud import storage -> from google.cloud import storage
+import pyrebase
+from config import FIREBASE_CONFIG
+from dataclasses import asdict, dataclass
+
+@dataclass
+class UserProperty:
+    email: str = 'unknown'
+    age_range: str = 'unknown'
+    gender: str = 'unknown'
+    phone_number: str = 'unknown'
+
+
+class DBModule:
+    def __init__(self):
+        firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
+        self.db = firebase.database()
+    
+    def register(self, platform_type, id, user_property: UserProperty):
+        info = asdict(user_property)
+        self.db.child("users").child(platform_type).child(id).set(info)
+
+    def verification(self, platform_type, id):
+        users = self.db.child("users").child(platform_type).get().val()
+        if users == None:
+            return False
+        
+        for user in users:
+            if user == str(id):
+                return True
+            
+        return False
+
+    def sign_in(self, platform_type, id, user_property: UserProperty = None):
+        if not self.verification(platform_type, id):
+            if user_property == None:
+                user_property = UserProperty()
+            self.register(platform_type, id, user_property)
+        return True
+
+    def save_test(self):
+        pass
+
+    def get_test(self, tid):
+        pass
+
+    def get_user(self, uid):
+        pass
--- flask/app.py
+++ flask/app.py
@@ -1,25 +1,34 @@
-from flask import redirect, url_for, session, request, jsonify, render_template, make_response
-from flask_jwt_extended import (
-    JWTManager, create_access_token, 
-    get_jwt_identity, jwt_required,
-    set_access_cookies, set_refresh_cookies, 
-    unset_jwt_cookies, create_refresh_token,
-    jwt_refresh_token_required
-)
+from flask import redirect, url_for, session, request, jsonify, make_response
+#from flask_jwt_extended import (
+#    JWTManager, create_access_token, 
+#    get_jwt_identity, jwt_required,
+#    set_access_cookies, set_refresh_cookies, 
+#    unset_jwt_cookies, create_refresh_token,
+#    jwt_refresh_token_required
+#)
 import requests
 from config import KAKAO_CLIENT_ID, KAKAO_SECRET, KAKAO_REDIRECT_URI, NAVER_CLIENT_ID, NAVER_SECRET, NAVER_REDIRECT_URI, GOOGLE_SECRET, GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI
-from model import UserData_N, UserModel, UserData_G, UserData_K
-from question import Question
+#from model import UserData_N, UserModel, UserData_G, UserData_K
+from DBhandler import DBModule, UserProperty
 from flask_app import *
 import logs
+from datetime import datetime, timedelta
 
 ### temporary server setup
-quest = Question()
+DB = DBModule()
+
+#Release: http://shift2me.com 
+#Debug: http://localhost:3000
+react_host = "http://localhost:3000"
 
 @app.route("/kakao_login")
 def kakao_login():
-    return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_CLIENT_ID}&redirect_uri={KAKAO_REDIRECT_URI}&prompt=login&response_type=code")
-
+    if "KAKAOTALK" in request.headers.get('User-Agent'):
+        #TODO: redirect to a react page that asserts change browser
+        return redirect(url_for(""), 302)
+    else:
+        return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={KAKAO_CLIENT_ID}&redirect_uri={KAKAO_REDIRECT_URI}&prompt=select_account&response_type=code")
+'''
 @app.route("/naver_login")
 def naver_login():
     return redirect(f"https://nid.naver.com/oauth2.0/authorize?client_id={NAVER_CLIENT_ID}&redirect_uri={NAVER_REDIRECT_URI}&auth_type=reauthenticate&response_type=code")
@@ -27,12 +36,15 @@ def naver_login():
 @app.route("/google_login")
 def google_login():
     return redirect(f"https://accounts.google.com/o/oauth2/auth/authorize?client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=profile+email&response_type=code")
+'''
 
 @app.route("/kakao_callback")
 def kakao_callback():
     code = request.args.get("code")
     if code:
-        token_req = requests.post("https://kauth.kakao.com/oauth/token", data={
+        token_req = requests.post("https://kauth.kakao.com/oauth/token", headers={
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        }, data={
             "grant_type": "authorization_code",
             "client_id": KAKAO_CLIENT_ID,
             "redirect_uri": KAKAO_REDIRECT_URI,
@@ -42,11 +54,45 @@ def kakao_callback():
         token_json = token_req.json()
 
         if "access_token" in token_json:
-            session["kakao_token"] = token_json["access_token"]
-            return redirect(url_for("kakao_profile"))
+            now = datetime.now()
+            #to get user information
+            my_info = requests.post("https://kapi.kakao.com/v2/user/me", headers={
+                "Authorization": f"Bearer {token_json['access_token']}",
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            })
+            my_info_json = my_info.json()
+            prop = UserProperty()
+            if "kakao_account" in my_info_json:
+                if "email" in my_info_json["kakao_account"] and my_info_json["kakao_account"]["is_email_valid"] and my_info_json["kakao_account"]["is_email_verified"]:
+                    prop.email = my_info_json["kakao_account"]["email"]
 
-    return "Failed to get Kakao access token."
+                if "age_range" in my_info_json["kakao_account"] :
+                    prop.age_range = my_info_json["kakao_account"]["age_range"]
 
+                if "gender" in my_info_json["kakao_account"]:
+                    prop.gender = my_info_json["kakao_account"]["gender"]
+
+                if "phone_number" in my_info_json["kakao_account"]:
+                    prop.phone_number = my_info_json["kakao_account"]["phone_number"]
+
+            DB.sign_in("KAKAO", my_info_json["id"], prop)
+            session["id"] = my_info_json["id"]
+            session["login_type"] = "KAKAO"
+            session["access_token"] = token_json["access_token"]
+            session["access_expires"] = now + timedelta(seconds=token_json["expires_in"])
+            session["refresh_token"] = token_json["refresh_token"]
+            session["refresh_expires"] = now + timedelta(seconds=token_json["refresh_token_expires_in"])
+
+            #session["expires_in"] = token_json["expires_in"]
+            #session["refresh_token_expires_in"] = token_json["refresh_token_expires_in"]
+            #session["login_time"] = datetime.now()
+            return redirect(f"{react_host}/host/info")
+
+    else:
+        error_type = request.args.get("error")
+        error_description = request.args.get("error_description")
+        return make_response(f"error: {error_type}\n description: {error_description}", 403)
+'''
 @app.route("/naver_callback")
 def naver_callback():
     code = request.args.get("code")
@@ -85,68 +131,27 @@ def google_callback():
             return redirect(url_for("google_profile"))
 
     return "Failed to get google access token."
-
-@app.route("/kakao_profile")
-def kakao_profile():
-    if "kakao_token" in session:
-        headers = {"Authorization": f"Bearer {session['kakao_token']}"}
-        profile_req = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
-        profile_json = profile_req.json()
-
-        user = UserData_K(profile_json)
-        UserModel().upsert_user(user)
-
-        return jsonify(profile_json)
-
-    return "Kakao login required."
-
-@app.route("/naver_profile")
-def naver_profile():
-    if "naver_token" in session:
-        headers = {"Authorization": f"Bearer {session['naver_token']}"}
-        profile_req = requests.get("https://openapi.naver.com/v1/nid/me", headers=headers)
-        profile_json = profile_req.json()
-
-        user = UserData_N(profile_json)
-        UserModel().upsert_user(user)
-
-        return jsonify(profile_json)
-
-    return "Naver login required."
-
-@app.route("/google_profile")
-def google_profile():
-    if "google_token" in session:
-        headers = {"Authorization": f"Bearer {session['google_token']}"}
-        profile_req = requests.get("https://www.googleapis.com/oauth2/v3/userinfo", headers=headers)
-        profile_json = profile_req.json()
-
-        user = UserData_G(profile_json)
-        UserModel().upsert_user(user)
-
-        return jsonify(profile_json)
-
-    return "Google login required."
+'''
 
 @app.route("/logout")
 def logout():
+    if not validate_token():
+        return make_response("already_logged_out", 403)
+
+    access_token = session["access_token"]
+    if "login_type" in session and session["login_type"] == "KAKAO":
+        requests.post("https://kapi.kakao.com/v1/user/logout", headers={
+            "Authorization": f"Bearer {access_token}"
+        })
+        #session.pop("access_token", None)
+        session.clear()
+        return redirect(react_host, 302)
+    '''
     if "naver_token" in session:
         session.pop("naver_token", None)
 
         response = make_response(redirect(url_for("index")))
         response.delete_cookie('naver_token')
-
-        return response
-
-    if "kakao_token" in session:
-        access_token = session["kakao_token"]
-        requests.post("https://kapi.kakao.com/v1/user/logout", headers={
-            "Authorization": f"Bearer {access_token}"
-        })
-        session.pop("kakao_token", None)
-
-        response = make_response(redirect(url_for("index")))
-        response.delete_cookie('logined')
 
         return response
 
@@ -162,9 +167,45 @@ def logout():
         response.delete_cookie('google_token')
 
         return response
+    '''
+    return make_response("failed_to_logout", 403)
 
+# This function is used to validate the token in the session
+# If there's no token in the session, it returns False, so you can use this function to check if the user is logged in
+# Also, this function includes a token validation process with _refresh_token() function
+def validate_token():
+    if "access_expires" not in session:
+        return False
+    elif datetime.now() > session["access_expires"]:
+        if "refresh_expires" in session and datetime.now() < session["refresh_expires"]:
+            return _refresh_token()
+        else:
+            session.clear()
+            return False
+    else:
+        return True
 
-    return "logout failed"
+#NOTE: DO NOT USE WITHOUT VALIDATING TOKEN
+def _refresh_token():
+    token_req = requests.post("https://kauth.kakao.com/oauth/token", headers={
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    }, data={
+        "grant_type": "refresh_token",
+        "client_id": KAKAO_CLIENT_ID,
+        "refresh_token": session["refresh_token"],
+        "client_secret": KAKAO_SECRET,
+    })
+    token_json = token_req.json()
+    if "access_token" in token_json:
+        now = datetime.now()
+        session["access_token"] = token_json["access_token"]
+        session["access_expires"] = now + timedelta(seconds=token_json["expires_in"])
+        session["refresh_token"] = token_json["refresh_token"]
+        session["refresh_expires"] = now + timedelta(seconds=token_json["refresh_token_expires_in"])
+        return True
+    else:
+        session.clear()
+        return False
 
 if __name__ == "__main__":
     app.run(debug=True)
