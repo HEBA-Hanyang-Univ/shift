f27c426b7450944fcadb5359df73d1163e211bfe
JUNStone <j00mst0m3@gmail.com>
2024-04-12 18:34:13 +0900

    remove React.StrictMode due to calling useEffect twice

--- react/src/index.js
+++ react/src/index.js
@@ -5,7 +5,5 @@ import "./assets/fonts/WantedSans.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
