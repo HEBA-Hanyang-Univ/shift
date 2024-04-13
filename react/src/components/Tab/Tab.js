4a4172a11a3c41ba518894e07eb28472d7794422
bigmooon <bigmooon1215@gmail.com>
2024-04-14 05:34:18 +0900

    add Result Detail page and Tab component

--- react/src/components/Tab/Tab.js
+++ react/src/components/Tab/Tab.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { ResultSummary } from "../../pages/Result/ResultSummary";
+import { ResultSummary } from "../../pages/Result/MZ/ResultSummary";
+import ResultDetail from "../../pages/Result/MZ/ResultDetail";
 import "./Tab.scss";
 
 export const Tab = () => {
   const [currentTab, setCurrentTab] = useState(0);
 
   const tabList = [
-    { name: '결과 요약', content: <ResultSummary /> },
-    { name: '자세히 보기', content: <div>상세 결과</div> },
+    { name: '결과 요약', content: <ResultSummary/> },
+    // TODO : meterValues 받아오기
+    { name: '자세히 보기', content: <ResultDetail meterValues={{MZ: 50, T: 50, R: 30, O: 25}}/>},
     { name: '응답자 통계', content: <div>준비중입니다.</div> }
   ]; 
 
