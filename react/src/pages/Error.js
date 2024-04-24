8aefc261eccf4570ea2be0e51471d8d41d2ecc04
bigmooon <bigmooon1215@gmail.com>
2024-04-24 15:55:51 +0900

    delete test code

--- react/src/pages/Error.js
+++ react/src/pages/Error.js
@@ -4,24 +4,7 @@ import { Link } from "react-router-dom";
 import { Button } from "../components/Button/Button";
 
 const Error = () => {
-  const [keywords, setKeywords] = useState({});
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/epa_keywords')
-      .then((response) => {
-        // 응답을 JSON으로 변환
-        return response.json();
-      })
-      .then((data) => {
-        // 변환된 JSON을 사용하여 상태 설정
-        setKeywords(data);
-      })
-      .catch((error) => {
-        // 오류 처리
-        console.error('Error!', error);
-      });
-  }, []);
-
+  
   return (
     <div id="Container" className="errorContainer">
       <div className="errorWrapper">
