bb8153f2c6383cd234dbae7d5ce8bd05b7453998
JUNStone <j00mst0m3@gmail.com>
2024-05-04 00:54:54 +0000

    modify flask_app's configuration

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from config import Flask_secret_key #,JWT_secret_key
+from config import Flask_secret_key, CORS_ORIGIN #,JWT_secret_key
 from flask_session import Session
 from datetime import timedelta
 #import os
@@ -17,5 +17,5 @@ app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 app.config['SESSION_PERMANENT'] = True
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
 #app.config['SESSION_FILE_THRESHOLD'] = 500 # default value is 500
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": CORS_ORIGIN}}, supports_credentials=True)
 Session(app)
