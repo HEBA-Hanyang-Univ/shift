bf367a1df9a755d64edce006a2fb230c5d831904
bigmooon <bigmooon1215@gmail.com>
2024-05-05 22:51:08 +0900

    modified some scss in Tab component

--- react/src/components/Tab/Tab.js
+++ react/src/components/Tab/Tab.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ResultSummary from "../../pages/Result/MZTabContent/ResultSummary";
 import ResultDetail from "../../pages/Result/MZTabContent/ResultDetail";
+import ResultStatistic from "../../pages/Result/MZTabContent/ResultStatistic";
 import "./Tab.scss";
 
 export const Tab = ({ data }) => {
@@ -10,7 +11,7 @@ export const Tab = ({ data }) => {
     { name: '결과 요약', content: <ResultSummary data={data} /> },
     // TODO : meterValues 받아오기
     { name: '자세히 보기', content: <ResultDetail data={data}/>},
-    { name: '응답자 통계', content: <div>준비중입니다.</div> }
+    { name: '응답자 통계', content: <ResultStatistic />}
   ]; 
 
   const selectMenuHandler = (index) => {
