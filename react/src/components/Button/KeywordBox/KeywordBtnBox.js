6e9988e24b90e298509c085f7a9500f9038ac9ea
JUNStone <j00mst0m3@gmail.com>
2024-05-15 01:32:53 +0900

    fix KeywordBtnBox component  - fix re-positioning when click buttons  - fix auto-scroll doesn't reach to the end of the right

--- react/src/components/Button/KeywordBox/KeywordBtnBox.js
+++ react/src/components/Button/KeywordBox/KeywordBtnBox.js
@@ -17,6 +17,12 @@ export const KeywordBtnBox = ({ keywords, width, height, className, onKeywordCli
       [keyword]:!selectedKeywords.includes(keyword)
     }),{});
     setActiveButtons(updatedActiveButtons);
+    keywordRowRefs.current.forEach((keywordRow, index) => {
+      if (keywordRow && backupScrollPositions[index] && backupScrollDirections[index]) {
+        keywordRow.scrollLeft = backupScrollPositions[index];
+        scrollDirection.current[index] = backupScrollDirections[index];
+      }
+    });
   }, [selectedKeywords, keywords]);
 
   // handleClick: 키워드 버튼 클릭 시 실행되는 함수
@@ -29,33 +35,42 @@ export const KeywordBtnBox = ({ keywords, width, height, className, onKeywordCli
     // 버튼 클릭 핸들러에서 상위 컴포넌트의 onKeywordClick 호출
     onKeywordClick(keyword);
     // 해당 키워드 버튼을 비활성화
+
     setActiveButtons(prev => ({...prev, [keyword]: !prev[keyword]}));
   };
 
   const entries = Object.entries(keywords);
   const chunks = [];
   const lines = Math.floor(entries.length / wordsPerLine);
-  const remain = entries.length % wordsPerLine;
   for (let i = 0; i < lines; i++) {
-    chunks.push(entries.slice(i*wordsPerLine, (i === lines-1)? (i+1)*wordsPerLine+remain : (i+1)*wordsPerLine));
+    chunks.push(entries.slice(i*wordsPerLine, (i+1)*wordsPerLine));
   }
+  if (entries.length % wordsPerLine > 0)
+    chunks.push(entries.slice((lines)*wordsPerLine, entries.length));
 
   const keywordRowRefs = useRef([]);
   const scrollDirection = useRef([]);
   const scrollPositions = useRef([]);
+
+  const backupScrollPositions = keywordRowRefs.current.map((keywordRow) => keywordRow.scrollLeft);
+  const backupScrollDirections = [...scrollDirection.current];
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       keywordRowRefs.current.forEach((keywordRow, index) => {
         if (keywordRow) {
           const scrollAmount = keywordRow.scrollLeft;
-          const maxScrollAmount = keywordRow.scrollWidth / 2;
+          // NOTE: scrollLeft's maximum value is scrollWidth - clientWidth,
+          // but it doesn't reach to the value. (I don't know why but it tends to be lower than maximum value.)
+          // So we need to subtract 1 to prevent the scrollDirection doesn't change though it's already scrolled to the end.
+          const maxScrollAmount = Math.floor(keywordRow.scrollWidth - keywordRow.clientWidth - 1);
 
           if (scrollAmount >= maxScrollAmount) {
             scrollDirection.current[index] = -0.5;
           } else if (scrollAmount <= 0) {
             scrollDirection.current[index] = 1;
           }
+
           keywordRow.scrollLeft = scrollAmount + scrollDirection.current[index];
           scrollPositions.current[index] = keywordRow.scrollLeft;
         }
@@ -69,8 +84,9 @@ export const KeywordBtnBox = ({ keywords, width, height, className, onKeywordCli
   useEffect(() => {
     // Restore the scroll positions after the component updates
     keywordRowRefs.current.forEach((keywordRow, index) => {
-      if (keywordRow && scrollPositions.current[index] !== undefined) {
-        keywordRow.scrollLeft = scrollPositions.current[index];
+      if (keywordRow && backupScrollPositions[index] && backupScrollDirections[index]) {
+        keywordRow.scrollLeft = backupScrollPositions[index];
+        scrollDirection.current[index] = backupScrollDirections[index];
       }
     });
   }, [activeButtons]);
@@ -83,9 +99,9 @@ export const KeywordBtnBox = ({ keywords, width, height, className, onKeywordCli
             keywordRowRefs.current[index] = el;
             scrollDirection.current[index] = 1;
             if (el && index % 2 === 0) {
-              el.scrollLeft = el.scrollWidth / 2;
-            } else if (el && index % 2 === 1) {
               el.scrollLeft = 0;
+            } else if (el && index % 2 === 1) {
+              el.scrollLeft = Math.floor(el.scrollWidth - el.clientWidth);
             }
           }}>
           {chunk.map(([keyword, value]) => (
